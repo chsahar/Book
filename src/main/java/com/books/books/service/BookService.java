33f38de9cf4bbf5b01bc51a1de81ebd0843b086e
chsahar <sahargithub@gmail.com>
2025-01-26 15:18:04 +0200

    Connected Database successfully

--- src/main/java/com/books/books/service/BookService.java
+++ src/main/java/com/books/books/service/BookService.java
@@ -1,10 +1,17 @@
 package com.books.books.service;
 
+import com.books.books.Book;
+import com.books.books.dao.BookDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
-    public String GetBook() {
-        return "Book";
-    }
+    @Autowired
+  BookDao bookDao;
+  public List<Book> getAllBooks() {
+    return bookDao.findAll();
+  }
 }
